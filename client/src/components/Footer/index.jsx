2f962114e18b01cb7a3b65ed970f4c21e76b04db
MFMcNolt <mfergusonmcnulty@gmail.com>
2024-01-30 20:14:43 +1100

    updated footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
           >
             ❤️
           </span>{' '}
-          by the Tech Thoughts team.
+          by your Neighbhours.
         </h4>
       </div>
     </footer>
